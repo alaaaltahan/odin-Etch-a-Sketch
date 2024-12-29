ecfdfbbba81b9c4c56a5623fbe344a1b604efeb3
alaa ahmad altahan <alaath777@gmail.com>
2024-12-29 05:06:16 +0300

    remove the console log and remove the numbers inside divs
    and add change the grid logic

--- script.js
+++ script.js
@@ -4,5 +4,36 @@ for(let i = 1 ; i <= 256 ; i++){
     let div  = document.createElement("div")
     div.textContent = i;
     container.appendChild(div)
-    console.log(i)
+    
 }
+
+let button = document.querySelector("#button")
+
+function changeGrid(){
+    let gridNumber = prompt("enter number between 1 and 100")
+    gridNumber = parseInt(gridNumber)
+    if (gridNumber > 100 || gridNumber < 1){
+        alert("please enter number between 1 and 100")
+    } else {
+        while(container.firstChild){
+            container.removeChild(container.lastChild)
+        }
+
+        for(let i = 1 ; i <= gridNumber * gridNumber ; i++){
+            let div  = document.createElement("div")
+            div.style.cssText = `
+                width: calc(100vw/${gridNumber});
+                height: calc(100vw/${gridNumber});
+                flex: 0 0 calc(100vw/${gridNumber}) ;
+            `
+           
+            container.appendChild(div)
+            
+        }
+
+
+
+    }
+
+}
+button.addEventListener("click", changeGrid)
